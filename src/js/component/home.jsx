015f0747160681d4555cb11ba871a27aa0f8d9ec
andresguerrah <andres.guerrah@gmail.com>
2022-07-06 01:09:49 +0000

    primer avance react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-
+import Card from "./Card.jsx";
+import Cardtitle from "./Cardtitle.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+
+
+
+<div className="row">
+  <div className="col-6 col-md-12"><Cardtitle/></div>
+  <div className="col-6 col-md-3"><Card/></div>
+  <div className="col-6 col-md-3"><Card/></div>
+  <div className="col-6 col-md-3"><Card/></div>
+  <div className="col-6 col-md-3"><Card/></div>
+</div>
 	);
 };
 

d2eaa9420b5f7dde3fb7d48279267470e00e1963
andresguerrah <andres.guerrah@gmail.com>
2022-07-07 23:09:24 +0000

    tarea 1 react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card.jsx";
 import Cardtitle from "./Cardtitle.jsx";
+import Nvar from "./Nvar.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -9,9 +10,9 @@ const Home = () => {
 	return (
 
 
-
-
-<div className="row">
+    <div className="row">
+  <div className="nvar"><Nvar/></div>
+ 
   <div className="col-6 col-md-12"><Cardtitle/></div>
   <div className="col-6 col-md-3"><Card/></div>
   <div className="col-6 col-md-3"><Card/></div>

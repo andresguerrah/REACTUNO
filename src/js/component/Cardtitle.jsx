667b91c47377861de7b1b20b34b40490fb099bf5
andresguerrah <andres.guerrah@gmail.com>
2022-07-08 01:18:20 +0000

    Tarea final SNK

--- src/js/component/Cardtitle.jsx
+++ src/js/component/Cardtitle.jsx
@@ -4,11 +4,11 @@ function Cardtitle() {
 return (
 
 <div className="card">
-  <h5 className="card-header">Featured</h5>
+  <h5 className="card-header">Mejores Personajes</h5>
   <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">Los mejores personajes de Shingeki no kiojin</h5>
+    <p className="card-text">Basado en la historia del anime a la fecha.</p>
+    <a href="#" className="btn btn-primary">Ver ultimo capitulo</a>
   </div>
 </div>
 );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -14,10 +14,10 @@ const Home = () => {
   <div className="nvar"><Nvar/></div>
  
   <div className="col-6 col-md-12"><Cardtitle/></div>
-  <div className="col-6 col-md-3"><Card/></div>
-  <div className="col-6 col-md-3"><Card/></div>
-  <div className="col-6 col-md-3"><Card/></div>
-  <div className="col-6 col-md-3"><Card/></div>
+  <div className="col-6 col-md-3"><Card p="El sacrificio mas importante." go="Ver +" titulo="Erwin Smith" img="https://nntheblog.b-cdn.net/wp-content/uploads/2022/04/APPEARANCE-Erwin-Smith-Aot.jpg"/></div>
+  <div className="col-6 col-md-3"><Card p="El mas fuerte de la humanidad." go="Ver +" titulo="Levi Ackerman" img="https://www.geekmi.news/__export/1646246937054/sites/debate/img/2022/03/02/levi-ackerman-1.jpg_982475294.jpg"/></div>
+  <div className="col-6 col-md-3"><Card p="Tiene todo en sus manos." go="Ver +" titulo="Eren Yeager" img="https://depor.com/resizer/X-xln6YiQqyEk2ddaDqZuQw9Ynw=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/Q7AHXEQYA5C7NMIDBAIN4H57O4.png"/></div>
+  <div className="col-6 col-md-3"><Card p="Importante en momentos clave." go="Ver +" titulo="Armin Arlert" img="http://pm1.narvii.com/6553/12c9a778d01c1d1f1bfffac430ab59d2071bde60_00.jpg" /></div>
 </div>
 	);
 };
